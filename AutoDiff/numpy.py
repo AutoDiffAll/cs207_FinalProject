fae97052b060521bb5cc89d357faf389707b09ae
Josh Feldman <joshfeldman@Joshs-MacBook-Pro-4.local>
2018-10-31 21:34:13 -0400

    added classes and methods

---
+++ AutoDiff/numpy.py
@@ -0,0 +1,6 @@
+# look in numpy for functions
+# functions should be able to handle inputs of Variable and regular python
+# numbers
+
+def sum():
+    
---
+++ AutoDiff/variables.py
@@ -0,0 +1,23 @@
+class Variable(object):
+    def __init__(self, val):
+        pass
+    
+    def der(self, dep_var):
+        pass
+    
+    def grad(self):
+        pass
+    
+    def __add__(self, other):
+        pass
+    
+    def __radd__(self, other):
+        pass
+    def __mul__(self, other):
+        pass
+    def __rmul__(self, other):
+        pass
+    
+    # implement other dunder methods for numbers
+    # https://www.python-course.eu/python3_magic_methods.php
+    

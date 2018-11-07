e07fa65f6f8b2d6803129f2009db9f165c1dd408
echoi3 <39250283+echoi3@users.noreply.github.com>
2018-11-06 21:35:37 -0500

    updated power function. Still working on pos and neg

--- AutoDiff/variables.py
+++ AutoDiff/variables.py
@@ -19,16 +19,17 @@ class Variable(object):
         except AttributeError:
             print("input is not a Variable")
         
-    
-    
     def grad(self):
         return self.der
     # unary operation of Variable instance.
+
+    def __pos__(self):
+        return Variable(self.name, self.val, der, False)
+
     def __neg__(self):
-        var = Variable(self.name, -self.val, self.der, False)    
-        for key in self.der:
-            var.der[key] = -self.der[key]
-        return var
+        der1 = self.der
+        new_der = {x: -der1.get(x,0) for x in set(der1)}
+        return Variable(self.name, -self.val, new_der, False)
 
     def __add__(self, other):
         der1=self.der
@@ -95,10 +96,32 @@ class Variable(object):
         der = {x: -other/self.val**2*der1.get(x, 0) for x in set(der1)}
         return Variable(self.name, other/self.val, der, False)
 
+    def __pow__(self, other):
+        try:
+            # calculate new derivative
+            new_der = {k: self.der.get(k, 0)*other.val*np.power(self.val, other.val-1) + other.der.get(k, 0)*np.log(self.val)*np.power(self.val, other.val) for k in set(self.der).union(other.der)}
+
+            new_name = "f({},{})".format(self.name, other.name)
+            new_val = np.power(self.val, other.val)
+
+            return Variable(new_name, new_val, new_der, False)
+        except AttributeError:
+            # only x is variable
+            if isinstance(self, Variable):
+                return Variable(self.name, np.power(self.val, other), {k:v*other*np.power(self.val, other-1) for (k,v) in self.der.items()}, False)
+            # only y is variable
+            elif isinstance(other, Variable):
+                return Variable(other.name, {k:v*np.log(self)*np.power(self, other.val) for (k,v) in other.der.items()})
+            # both not variable
+            else:
+                return np.power(self, other)
+    
     def jacobian(self):
         der1 = self.der
         jacobian = {key: self.der[key] for key in set(der1)}
         return jacobian
+
+    
 
 
 

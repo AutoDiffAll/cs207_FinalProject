b34cfdafbbb74cd8e269765c1f7c420df541742b
echoi3 <39250283+echoi3@users.noreply.github.com>
2018-12-09 11:13:20 -0500

    updated steepest descent

--- AutoDiff/Optimizer.py
+++ AutoDiff/Optimizer.py
@@ -124,8 +124,8 @@ def min_conjugate_gradient(fn, x0, precision=1e-5, max_iter=10000):
         beta = (g @ g) / (g @ g)
         s = g + beta*s
         argmin_fn = lambda alpha: fn(*[i + alpha*j for i, j in zip(x, s)])
-        
         alpha = minimize(argmin_fn, 0).x
+        
         x = x + alpha*s
 
         # store history of values
@@ -171,7 +171,6 @@ def min_gradientdescent(fn, x0, precision, max_iter, lr=0.01):
         x_var = []
         for i, v in enumerate(x):
             x_var.append(Variable(var_names[i], v))
-
         # obtain values and jacobian to find delta_f
         val_vector = np.array([value.val for value in x_var])
         jacobian = np.array([fn(*x_var).der.get(i) for i in var_names])
@@ -194,6 +193,63 @@ def min_gradientdescent(fn, x0, precision, max_iter, lr=0.01):
         # iteration stopping condition
         if nums_iteration >= max_iter:
             return Result(x, val_rec, time_rec, False)
+        nums_iteration +=1
+    
+def min_steepestdescent(fn, x0, precision, max_iter, lr=0.01):
+     # create initial variables
+    # right now we only test with the 26 alphabets
+    from string import ascii_lowercase
+    import time
+    import numpy as np
+    from scipy.optimize import fmin
+
+    name_ls = iter(ascii_lowercase)
+
+    # create initial variables
+    var_names = []
+    for i in x0:
+        name = next(name_ls)
+        var_names.append(name)
+
+    x = np.array(x0)
+    s = 0 # initialize as 0 works to ensure that s=g in 1st iteration
+
+    nums_iteration = 0
+    val_rec = []
+    time_rec = []
+    init_time = time.time()
+     # initial guess of n = 0.01
+    n = 0.01
+    while True:
+        # recreate new variables with new values
+        x_var = []
+        for i, v in enumerate(x):
+            x_var.append(Variable(var_names[i], v))
+        # obtain values and jacobian to find delta_f
+        val_vector = np.array([value.val for value in x_var])
+        jacobian = np.array([fn(x_var).der.get(i) for i in var_names])
+        delta_f = jacobian*val_vector
+        
+       
+        find_min = fmin(fn, val_vector-n*delta_f, maxiter = 1, disp=False)
+        n = (find_min - x)/delta_f
+
+        # update x
+        old_x = x
+        x = x + n*delta_f
+
+        # threshold stopping condition
+        if max(abs(x-old_x)) < precision:
+            return Result(x, val_rec, time_rec, True)
+
+        # store history of values
+        val_rec.append(x)
+      
+        time_rec.append(time.time()-init_time)
+        
+        # iteration stopping condition
+        if nums_iteration >= max_iter:
+            return Result(x, val_rec    , time_rec, False)
         nums_iteration +=1
 
 

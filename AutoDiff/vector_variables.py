73b1b5b3d8548fc08c9bef7ff8f7d54ea6a5ceb4
Shane <shengsiongong@g.harvard.edu>
2018-12-10 14:39:38 -0500

    updated vector_variables

--- AutoDiff/vector_variables.py
+++ AutoDiff/vector_variables.py
@@ -66,6 +66,69 @@ class vector_Variable(object):
     def __neg__(self):
         return vector_Variable(np.negative(self.variables))
 
+def vectorize_variable(fn):
+    """Given a vector function of variables, returns a function that
+    wraps the original function to return a new vector_Variable class
+    that can be used to extract the values and the jacobian of the vector
+    easily
+
+    INPUTS
+    =======
+    fn: predefined vector function that can take in any number of inputs
+       Vector function that user wants to apply on variables
+
+    RETURNS
+    ========
+    fn_wrapper: wrapped function that takes in any number of inputs
+
+    EXAMPLES
+    =========
+    >>> try:
+    ...     from variables import Variable
+    ... except:
+    ...     from AutoDiff.variables import Variable
+    >>> try:
+    ...     from vector_variables import vectorize_variable
+    ... except:
+    ...     from AutoDiff.vector_variables import vectorize_variable
+    >>> try:
+    ...     import AD_numpy as anp
+    ... except:
+    ...     import AutoDiff.AD_numpy as anp
+    >>> @vectorize_variable
+    ... def vec_fn(x, y, z):
+    ...     f1 = x * y + anp.sin(y) + anp.cos(z)
+    ...     f2 = x + y + anp.sin(x*y)
+    ...     return [f1,f2]
+    >>> a = Variable('a', 3)
+    >>> b = Variable('b', 1)
+    >>> c = Variable('c', 2)
+    >>> f = vec_fn(a, b, c)
+    >>> f.jacobian().values
+    array([[ 1.        ,  3.54030231, -0.90929743],
+           [ 0.0100075 , -1.96997749,  0.        ]])
+    >>> f.val
+    array([3.42532415, 4.14112001])
+    """
+    def fn_wrapper(*args):
+        variable_vec = fn(*args)
+
+        # if every object in vector is just numeric
+        if all([isinstance(i, (int, float, complex)) for i in variable_vec]):
+            return variable_vec
+
+        # check that it is not single variable
+        if isinstance(variable_vec, Variable):
+            raise TypeError('Function is not a vector function!')
+
+        # check that every object in vector is a variable
+        # OR SHOULD WE ALLOW SUCH THAT NOT ALL OBJECT IS A VARIABLE?
+        if not all([isinstance(i, Variable) for i in variable_vec]):
+            raise TypeError('Every object in a vector function should be a Variable!')
+
+        return vector_Variable(variable_vec)
+    return fn_wrapper
+
 
 if __name__ == "__main__":
     import doctest

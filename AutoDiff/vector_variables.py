571da66cce4f5c44f0fb318241d45adfd8980e1e
Shane <shengsiongong@g.harvard.edu>
2018-12-10 22:16:59 -0500

    updated doctest for vector_variable

--- AutoDiff/vector_variables.py
+++ AutoDiff/vector_variables.py
@@ -17,9 +17,96 @@ class vector_Variable(object):
          sort=True, ignore_index=True).fillna(0)
 
     def jacobian(self):
+        """Returns jacobian of variable
+
+
+        RETURNS
+        ========
+        value: pandas dataframe
+
+        NOTES
+        =====
+        POST:
+            - returns jacobian as a dataframe where columns are dependent variable
+
+        EXAMPLES
+        =========
+        >>> try:
+        ...     from variables import Variable
+        ... except:
+        ...     from AutoDiff.variables import Variable
+        >>> try:
+        ...     from vector_variables import vectorize_variable
+        ... except:
+        ...     from AutoDiff.vector_variables import vectorize_variable
+        >>> try:
+        ...     import AD_numpy as anp
+        ... except:
+        ...     import AutoDiff.AD_numpy as anp
+        >>> @vectorize_variable
+        ... def vec_fn(x, y, z):
+        ...     f1 = x * y + anp.sin(y) + anp.cos(z)
+        ...     f2 = x + y + anp.sin(x*y)
+        ...     return [f1,f2]
+        >>> a = Variable('a', 2)
+        >>> b = Variable('b', 3)
+        >>> c = Variable('c', 2)
+        >>> f = vec_fn(a, b, c)
+        >>> f.jacobian().values
+        array([[ 1.        ,  3.54030231, -0.90929743],
+               [ 0.0100075 , -1.96997749,  0.        ]])
+        """
         return self.der
 
     def partial_der(self, dep_var):
+        """Returns partial derivative of a variable w.r.t. another variable.
+
+        INPUTS
+        =======
+        dep_var: Variable
+
+        RETURNS
+        ========
+        value: numeric, element-wise for lists, arrays, or similar structures
+
+        NOTES
+        =====
+        POST:
+            - if this variable is not a function of dep_var, return 0
+            - if dep_var is not a variable, raise AttributeError
+
+        EXAMPLES
+        =========
+        >>> try:
+        ...     from variables import Variable
+        ... except:
+        ...     from AutoDiff.variables import Variable
+        >>> try:
+        ...     from vector_variables import vectorize_variable
+        ... except:
+        ...     from AutoDiff.vector_variables import vectorize_variable
+        >>> try:
+        ...     import AD_numpy as anp
+        ... except:
+        ...     import AutoDiff.AD_numpy as anp
+        >>> @vectorize_variable
+        ... def vec_fn(x, y, z):
+        ...     f1 = x * y + anp.sin(y) + anp.cos(z)
+        ...     f2 = x + y + anp.sin(x*y)
+        ...     return [f1,f2]
+        >>> a = Variable('a', 2)
+        >>> b = Variable('b', 3)
+        >>> c = Variable('c', 2)
+        >>> f = vec_fn(a, b, c)
+        >>> x.partial_der(a)
+        array([1.       , 0.0100075])
+        >>> d = 3
+        >>> x.partial_der(b)
+        input is not a Variable
+        >>> e = Variable('e', 3)
+        >>> x.partial_der(e)
+        array([0., 0.])
+        """
         try:
             if dep_var.name not in self.der.columns.values:
                 return np.zeros(self.der.shape[0])
